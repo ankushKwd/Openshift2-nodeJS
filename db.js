975e141871b95481f12c975e0c9887ae6b9d46fa
Ankush Kawde <mr.kawde@gmail.com>
2023-01-08 18:55:44 +0530

    updated password

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 function Conn () {
-    const mongoDB = "mongodb+srv://ankukwd_mongo:AkDb1234@cluster0.6pc8mon.mongodb.net/?retryWrites=true&w=majority";
+    const mongoDB = "mongodb+srv://ankukwd_mongo:<password>@cluster0.6pc8mon.mongodb.net/?retryWrites=true&w=majority";
     return mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 module.exports = Conn;
