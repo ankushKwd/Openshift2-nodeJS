65b21e6ecd45a20726269e576abaa7f60c9f01e2
Ankush Kawde <mr.kawde@gmail.com>
2023-01-08 18:44:01 +0530

    updated mongo details

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 function Conn () {
-    const mongoDB = "mongodb://127.0.0.1/AkkiLearning";
+    const mongoDB = "mongodb+srv://ankukwd_mongo:AkDb1234@cluster0.6pc8mon.mongodb.net/?retryWrites=true&w=majority";
     return mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 module.exports = Conn;
